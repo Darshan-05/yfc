f0a440ebc02ec061c44a3848c274be68ff105ebe
Darshan <56391013+Darshan-05@users.noreply.github.com>
2020-07-02 20:39:10 +0530

    code1.1

--- yfc.js
+++ yfc.js
@@ -1,1 +1,11 @@
-console.log('hai Iam darshan');
+(function () {
+      var words = [
+            'CHANGE', 'CREATOR', 'FUTURE'
+      ], i = 0;
+      setInterval(function () {
+            $('#changingword').fadeOut(function () {
+                  $(this).html(words[i = (i + 1) % words.length]).fadeIn();
+            });
+      }, 1000);
+
+})();
