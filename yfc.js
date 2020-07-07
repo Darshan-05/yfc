76fdec926e8a42eb0adb81b2b0bc31088bdc6a6d
Darshan <56391013+Darshan-05@users.noreply.github.com>
2020-07-07 13:25:53 +0530

    code1.1.3

--- yfc.js
+++ yfc.js
@@ -1,10 +1,9 @@
 (function () {
       var words = [
-            'CHANGE', 'CREATOR', 'FUTURE'
-      ], i = 0;
+            'change', 'creator', 'future'], i = 0;
       setInterval(function () {
             $('#changingword').fadeOut(function () {
                   $(this).html(words[i = (i + 1) % words.length]).fadeIn();
             });
-      }, 1000);
+      }, 1500);
 })();
