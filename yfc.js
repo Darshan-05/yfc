4cbd2cad8933c452041d030eca2e89c820b90b7b
Lawliet7 <darundarshan1223@gmail.com>
2020-12-13 21:05:24 +0530

    del

--- yfc.js
+++
@@ -1,9 +0,0 @@
-(function () {
-      var words = [
-            'change', 'creator', 'future'], i = 0;
-      setInterval(function () {
-            $('#changingword').fadeOut(function () {
-                  $(this).html(words[i = (i + 1) % words.length]).fadeIn();
-            });
-      }, 1500);
-})();
