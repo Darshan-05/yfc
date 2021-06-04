baaec361bc3083a11d10d6b0edffdd270a5d0dd5
Lawliet7 <darundarshan1223@gmail.com>
2021-06-04 07:00:46 +0530

    ...

--- style.js
+++ style.js
@@ -1,6 +1,8 @@
 (function () {
       var words = [
-            'change', 'creator', 'future'], i = 0;
+                  'change', 'creator', 'future'
+            ],
+            i = 0;
       setInterval(function () {
             $('#changingword').fadeOut(function () {
                   $(this).html(words[i = (i + 1) % words.length]).fadeIn();
